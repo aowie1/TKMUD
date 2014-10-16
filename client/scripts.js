4667b7801f846d28108c8794faa9cff04c56de43
Kyle Fredin <kylefredin@gmail.com>
2014-10-15 20:36:33 -0600

    adds message object

--- client/scripts.js
+++ client/scripts.js
@@ -1,4 +1,5 @@
-var socket = new WebSocket('url');
+var socket = new WebSocket('url'),
+    message;
 
 socket.onopen(function (event) {
     console.log('socket open');
@@ -8,4 +9,24 @@ socket.onmessage(function (event) {
     console.log('message', event.data);
 });
 
-socket.send('yo');
+function Message () {
+    this.command = null;
+    this.user = null;
+    return this;
+};
+
+Message.prototype.setCommand = function (command) {
+    this.command = command;
+    return this;
+};
+
+Message.prototype.setUser = function (user) {
+    this.user = user;
+    return this;
+};
+
+message = new Message();
+message.setCommand('LOGIN');
+message.setUser('someone');
+
+socket.send(message);
